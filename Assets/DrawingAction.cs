43625790d36a543e409d3ba32372a1c192db4e1c
Gamer Seb <strelles@gmail.com>
2014-05-26 02:19:58 +0200

    no message

---
+++ Assets/DrawingAction.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawingAction : Action {
+	public GameObject drawingOne;
+
+	private Color colorToFadeOut;
+
+	private bool running=false;
+	private float a=0f;
+
+	public override void execute(){		
+		running = true;	
+		colorToFadeOut = drawingOne.renderer.material.color;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (running) {
+			colorToFadeOut.a-=Time.deltaTime*0.5f;
+			if(colorToFadeOut.a<=0f){
+				colorToFadeOut.a=0f;
+				running=false;
+			}
+
+			drawingOne.renderer.material.color=colorToFadeOut;
+		}
+	}
+}
---
+++ Assets/GameObjectAppearDisappearAction.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjectAppearDisappearAction : Action {
+	
+	public override void execute(){		
+		GetComponent<GameObjectDestroyer> ().Destroy ();
+		
+	}
+}
